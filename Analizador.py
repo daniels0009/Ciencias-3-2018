d8dcb136c94d4ba66bb87995fdcf990f78d9d0e8
eduardpii <eduardp96@gmail.com>
2018-03-12 22:59:38 -0500

    Update Analizador.py

--- Analizador.py
+++ Analizador.py
@@ -5,7 +5,11 @@ import sys
 cuentaNumeros=0
 aux = 0
 pila = Pila ()
-        
+valor = []
+variable = []
+operador = []
+tokens = {}
+                
 def convertir(lista, pila,cuentaNumeros):
     if lista != []:
         if lista[0] in "+-*/=":
@@ -55,15 +59,40 @@ def verificaCaracter(lista):
         return True
     else:
         return False
-    
+ 
+def patrones(expresion):
+    error = 0
+    patronNum = re.compile('^[-+]?[0-9]+$')
+    patronVar = re.compile('^[a-z][a-zA-Z_$0-9]*$')
+    patronOpe = re.compile('[-|+|*|/|=]')
+    for i in expresion:
+        if(patronNum.match(i)):
+            #print("Num "+ i)
+            valor.append(i)
+            tokens["Valor"]= valor
+        elif(patronVar.match(i)):
+            #print("Var "+ i)
+            variable.append(i)
+            tokens["Variable"]= variable
+        elif(patronOpe.match(i)):
+            #print("Ope "+ i)
+            operador.append(i)
+            tokens["Operador"]= operador
+        else:
+            error += 1
+    return error
+
 def ejecutar(lista):
     if lista != []:
-        if(convertir(lista[0],pila,cuentaNumeros) and verificaCaracter(lista[0])):
-            print evaluar(pila.desapilar())
+        if (patrones(lista[0])== 0):
+            if(convertir(lista[0],pila,cuentaNumeros) and verificaCaracter(lista[0])):
+                print (evaluar(pila.desapilar()))
+            else:
+                print("Error Semantico")
+            return ejecutar(lista[1:])
         else:
-            print("error")
+            print("Error de Escritura")
         return ejecutar(lista[1:])
-        
     
     
 
