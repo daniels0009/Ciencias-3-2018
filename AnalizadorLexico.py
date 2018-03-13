6bb4b36c3e6d999097303fd2357a7d465712d792
eduardpii <eduardp96@gmail.com>
2018-03-12 19:44:40 -0500

    Update AnalizadorLexico.py

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -6,39 +6,39 @@ from arbol import *
 #Esta función convierte la lista, en una pila.
 def convertir (lista, pila):
     si lista! = []:
-        si lista [0] en "+ - * /": # evalúa los operadores
+        if lista [0] in "+ - * /": # evalúa los operadores
             nodo_der = pila.desapilar () # Desapila debido a la posfija
             nodo_izq = pila.desapilar () # Desapila debido a la posfija
             pila.apilar (Nodo (lista [0], nodo_izq, nodo_der)) # apila arbol
-        elif lista [0] en "0123456789":
+        elif lista [0] in "0123456789":
             imprimir ("entra")
             valor = lista [0]
             pila.apilar (Nodo (valor [0]))
-        elif lista [0] en "=":
+        elif lista [0] in "=":
             variable = pila.desapilar (). valor
             variables [variable] = [evaluar (pila.desapilar ())]
             print (variable + "=" + str (variables [variable] [0]))
-        más:
+        else:
             pila.apilar (Nodo (lista [0])) # apila nodos
         return convertir (lista [1:], pila) #recursividad
     
 #Esta función resuelve el árbol           
 def evaluar (arbol):
-    si arbol.valor == "+":
+    if arbol.valor == "+":
         return evaluar (arbol.izq) + evaluar (arbol.der)
-    si arbol.valor == "-":
+    if arbol.valor == "-":
         return evaluar (arbol.izq) - evaluar (arbol.der)
-    si arbol.valor == "/":
+    if arbol.valor == "/":
         return evaluar (arbol.izq) / evaluar (arbol.der)
-    si arbol.valor == "*":
+    if arbol.valor == "*":
         return evaluar (arbol.izq) * evaluar (arbol.der)
     return int (arbol.valor)
     
 def main ():
     lista = []
-    Pila = Pila ()
-    archivo = abrir ('archivo.txt', 'r')
-    para linea en archivo.readlines ():
+    pila = Pila ()
+    archivo = open ('archivo.txt', 'r')
+    for linea in archivo.readlines ():
         expresion = linea.split ("")
         lista.append (expresion)
     archivo.close ()
@@ -46,4 +46,4 @@ def main ():
    
     
 si __name__ == "__main__":
-    principal()
+    main()
